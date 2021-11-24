66b1e12eb19518e2dad333a34a338235e1205184
NicolasDed <nicolas.dedoyard@student.vinci.be>
2021-11-24 17:29:02 +0100

    test example

--- BerlinClockKataTest.php
+++ BerlinClockKataTest.php
@@ -2,8 +2,17 @@
 
 
 use PHPUnit\Framework\TestCase;
+require 'BerlinClockKata.php';
 
 class BerlinClockKataTest extends TestCase
 {
+    public function test()
+    {
+        // arrange
 
+        // act
+
+        // assert
+
+    }
 }
