3ebe7dc9b98bc038666c3756958b4b8c00840856
NicolasDed <nicolas.dedoyard@student.vinci.be>
2021-11-24 17:38:54 +0100

    constructor BerlinClockKata

--- BerlinClockKata.php
+++ BerlinClockKata.php
@@ -2,5 +2,11 @@
 
 class BerlinClockKata
 {
+    public $hours, $minutes, $seconds;
 
+    public function __construct(int $hours, int $minutes, int $seconds) {
+        $this->hours = hours;
+        $this->minutes = minutes;
+        $this->seoonds = seconds;
+    }
 }
