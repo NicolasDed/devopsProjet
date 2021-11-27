e8908624ed05a3b841ee73eb295c4537c01a7108
laurent <laurent.vandermeersch@student.vinci.be>
2021-11-27 12:39:16 +0100

    ajout singleminutesLamp

--- BerlinClockKata.php
+++ BerlinClockKata.php
@@ -12,11 +12,24 @@ class BerlinClockKata
 
     public function singleMinutesLamps()
     {
+        if($this->minutes %5 != 0) {
+            if ($this->minutes % 5 == 4)
+                echo "Y Y Y Y O<br>";
+            else if ($this->minutes % 5 == 3)
+                echo "Y Y Y O O<br>";
+            else if($this->minutes %5 == 2)
+                echo "Y Y O O O<br>";
+            else if($this->minutes %5 == 1)
+                echo "Y O O O O<br>";
+        }
+        echo "O O O O O<br>";
         return 0;
     }
 
     public function fiveMinutesLamps()
     {
+        if($this->minutes %5 == 0)
+            return
         return 0;
     }
 
@@ -34,10 +47,10 @@ class BerlinClockKata
     {
         if ($this->seconds %2 == 0){
             echo "Y<br>";
-            return 0;
+            return 1;
         }
         echo "O<br>";
-        return 1;
+        return 0;
     }
 
 
